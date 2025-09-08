cbda42b16ad0bdf85440a0e08a97a59d472e9c84
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-08 10:52:54 -0400

    Criando o gerar ID e buscar no array

--- src/controller/LivroController.ts
+++ src/controller/LivroController.ts
@@ -2,7 +2,10 @@ import { Livros } from "../model/Livros";
 import { LivroRepository } from "../repository/LivroRepository";
 
 export class LivroController implements LivroRepository{
-    
+
+    private listaLivros: Array<Livros> = new Array<Livros>();
+    id: number = 0;
+
     procurarPorNumero(id: number): void {
         throw new Error("Method not implemented.");
     }
@@ -17,6 +20,20 @@ export class LivroController implements LivroRepository{
     }
     deletar(id: number): void {
         throw new Error("Method not implemented.");
+    }
+
+    public gerarId(): number{
+        return ++ this.id;
+    }
+
+    public buscarNoArray(id: number): Livros | null {
+        for(let livro of this.listaLivros){
+            if(livro.id === id){
+                return livro;
+            }
+        }
+
+        return null;
     }
 
 }
