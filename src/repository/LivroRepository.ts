0398352c3fffc746f491b80ee5e8a935b3d91d4f
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-08 11:12:05 -0400

    Tirando o MVC dessa branch

--- src/repository/LivroRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Livros } from "../model/Livros"
-
-export interface LivroRepository{
-    procurarPorNumero(id: number): void;
-    listarTodos(): void;
-    cadastrar(livro: Livros): void; 
-    atualizar(livro: Livros): void; 
-    deletar(id: number): void; 
-}
