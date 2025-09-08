f11fecedda4263cad2a2d6fcc2fe2bcff978cb68
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-09-08 09:05:08 -0400

    Criação do menu

--- Menu.ts
+++ Menu.ts
@@ -1,1 +1,86 @@
 import leia = require('readline-sync');
+
+export function main(){
+
+    let opcao;
+
+    while (true){
+
+        console.log('*******************************************');
+        console.log('                                           ');
+        console.log('           📚 Livraria Online             ');
+        console.log('                                           ');
+        console.log('*******************************************');
+        console.log('                                           ');
+        console.log('          1 - Cadastrar Livro              ');
+        console.log('          2 - Listar todos os livros       ');
+        console.log('          3 - Buscar livro pelo número     ');
+        console.log('          4 - Atualizar dados de um livro  ');
+        console.log('          5 - Excluir um livro             ');
+        console.log('          6 - Sair                         ');
+        console.log('                                           ');
+        console.log('*******************************************');
+        console.log('                                           ');
+
+
+        console.log('Entre com a opção desejada: ');
+        opcao = leia.questionInt('');
+
+        if(opcao === 6){
+            console.log('\nLivraria Online agradece e te deseja uma ótima leitura!');
+            sobre();
+            process.exit(0);
+        }
+
+        switch(opcao){
+            case 1: 
+                console.log('\n\nCadastrar Livro\n\n');
+
+                keyPress();
+                break;
+            case 2:
+                console.log('\n\nListar todos os livros\n\n');
+
+                keyPress();
+                break;
+            case 3:
+                console.log('\n\nBuscar livro pelo número\n\n');
+                
+                keyPress();
+                break;
+            case 4:
+                console.log('\n\nAtualizar dados do livro\n\n');
+
+                keyPress();
+                break;
+            case 5:
+                console.log('\n\nDeletar Livro\n\n');
+
+                keyPress();
+                break;
+            default:
+                console.log('Opção Inválida!');
+
+                keyPress();
+                break;
+
+        }
+
+
+    }
+} 
+
+function sobre(): void{
+    console.log('\n*****************************************************');
+    console.log('Projeto Desenvolvido por: ');
+    console.log('Elzilane Barreto - barretoelzilane@gmail.com');
+    console.log('https://github.com/elzilanebarreto/projeto-ecommerce');
+    console.log('*****************************************************');
+}
+
+function keyPress(): void{
+    console.log('\nPressione enter para continuar...');
+    leia.prompt();
+}
+
+main();
